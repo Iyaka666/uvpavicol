5c033fb158949290b9ef953ba621e87623578eb1
Iyaka666 <duendecum@gmail.com>
2022-10-31 17:03:55 -0500

    i added verification of value valid

--- src/modelo/Alimento.java
+++ src/modelo/Alimento.java
@@ -1,19 +1,29 @@
 package modelo;
 
+import util.exceptions.StringVoidException;
+import util.exceptions.NegativeInvalidException;
+
 /**
  *
  * @author Marlon E. Zambrano Z.
- * @author Juan 
+ * @author Juan
  * @author Santiago G.
- * 
+ *
  */
 public class Alimento {
+
     private String tipo;
     private short cantidad;
 
-    public Alimento(String tipo, short cantidad) {
+    public Alimento(String tipo, short cantidad) throws Exception {
+        if ("".equals(tipo.trim())) {
+            throw new StringVoidException("El tipo no puede estar vacio");
+        }
+        if (!(cantidad > 0)) {
+            throw new NegativeInvalidException("La cantidad no puede ser un numero negativo");
+        }
         this.tipo = tipo;
         this.cantidad = cantidad;
     }
-    
+
 }
