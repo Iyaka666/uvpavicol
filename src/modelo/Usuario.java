06d3920f59a1fa2efa846bb91c9b5a886ce3b126
Iyaka666 <duendecum@gmail.com>
2022-10-31 10:10:25 -0500

    i modified atribbute name

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -5,13 +5,17 @@ package modelo;
  * @author Marlon E. Zambrano Z.
  */
 public class Usuario extends Persona{
-    private String nombre;
+    /*
+    Se omite este atributo modificandolo del diagrama de clases ya que el nombre completo de la
+    persona se puede establecer como nombre de usuario
+    */
+    //private String nombre;
+    
     private Contrasenia contrasenia;
     private Granja granjaAcargo;
 
-    public Usuario(String nombre, Contrasenia contrasenia, Granja granjaAcargo, long nuip, String nombreCompleto, String correoElectronico) {
+    public Usuario(Contrasenia contrasenia, Granja granjaAcargo, long nuip, String nombreCompleto, String correoElectronico) {
         super(nuip, nombreCompleto, correoElectronico);
-        this.nombre = nombre;
         this.contrasenia = contrasenia;
         this.granjaAcargo = granjaAcargo;
     }
