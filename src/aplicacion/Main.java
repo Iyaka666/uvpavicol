1aa395bebc6df42a9b07b86ba1ac0eb091ed80a2
Iyaka666 <duendecum@gmail.com>
2022-11-30 16:55:35 -0500

    rename package

---
+++ src/aplicacion/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplicacion;
+
+/**
+ *
+ * @author Marlon E. Zambrano Z.
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
