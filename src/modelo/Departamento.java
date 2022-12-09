839cf5976a231e5eb3b8944c41640b05afb201e3
Iyaka666 <duendecum@gmail.com>
2022-12-08 21:43:53 -0500

    i added info

--- src/modelo/Departamento.java
+++ src/modelo/Departamento.java
@@ -6,8 +6,19 @@ package modelo;
  * @author Juan E. Rodriguez R.
  */
 public class Departamento {
-
-    public Departamento() {
+    private String nombre;
+    
+    public Departamento( String nombre) {
+        this.nombre = nombre;
     }
+
+    @Override
+    public String toString() {
+        return this.nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }  
     
 }
